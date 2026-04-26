bba1b01dfac6b70dacbe63c179efc040a18b6683
Javier Pulido <fj.pulidop@gmail.com>
2026-04-26 21:55:20 +0200

    Add desktop update toast and updater release assets

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -87,6 +87,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(move |app| {
             let app_handle = app.handle().clone();
 
